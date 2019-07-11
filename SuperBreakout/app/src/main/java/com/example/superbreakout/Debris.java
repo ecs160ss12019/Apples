e0a05280a7fb26be83851a75cc5c6c89e9aa2647
billpansacola <billmatthew.pansacola@gmail.com>
2019-07-10 21:32:45 -0700

    Updated Debris class with backbone functions.

--- SuperBreakout/app/src/main/java/com/example/superbreakout/Debris.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/Debris.java
@@ -1,5 +1,45 @@
 package com.example.superbreakout;
+import android.graphics.RectF;
 
+/*
+ *  Falling objects that can destroy the bat, or grant either an upgrade or downgrade
+ */
 public class Debris {
-    //Falling upgrades or downgrades
+
+    private final int SPEED = 1; // Constant speed (in Y-direction) of debris
+
+    private RectF debris; // Square/Rect representing the falling debris
+    private float width;  // Debris' width in terms of pixels
+    private float height; // Debris' height in terms of pixels
+
+    private float xPos; // x coordinate of debris
+    private float yPos; // y coordinate of debris
+
+    private String debrisType; // Debris type
+
+    public Debris() {
+
+    }
+
+    public void spawnDebris() {
+
+    }
+
+    public void fall() {
+
+    }
+
+    public void destroy() {
+
+    }
+
+    public void getDebrisType() {
+
+    }
+
+    public void hit() {
+
+    }
+
+
 }
