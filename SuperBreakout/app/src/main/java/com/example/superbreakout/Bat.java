822f0cb0867a9c73824e26206ffa2d7b60aeb568
annguyen120 <antnguye@ucdavis.edu>
2019-07-10 18:35:13 -0700

    Barebone Bat class

--- SuperBreakout/app/src/main/java/com/example/superbreakout/Bat.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/Bat.java
@@ -1,4 +1,55 @@
 package com.example.superbreakout;
 
+import android.graphics.RectF;
+
 public class Bat {
+	
+	public final int LEFT = -1;
+	public final int STOP = 0;
+   	public final int RIGHT = 1;	
+
+	RectF rect; // Object to represent Bat's four corners
+	
+	private float x; // Bottom left coordinate of the Bat
+	private float y; // Top coordinate of the Bat
+	
+	private float width; // Width of the bat
+	private float height; // Height of the bat
+
+	private float speed; // Speed of the Bat
+	private int direction; // The bat's current direction
+	
+	/*
+	 * @posX - Screen's width
+	 * @posY - Screen's height
+	 * 
+	 * Instantiate Bat object and place bat's coordinate in the bottom center
+	 * Instantiate rect with x, y, width, and height to represent Bat's 4
+	 * corners.
+	 */
+	public Bat(int posX, int posY){
+	}
+	
+	// Return rect object that represents Bat's 4 corner object
+	public RectF getRect(){ return rect;}
+	
+	// Return Bat's direction
+	public int getDirection(){return direction;}
+	
+	/*
+	 * @direction - change direction of the bat
+	 *
+	 * setDirection will take change the direction of the Bat, either
+	 * moving left, stopping, or moving right.
+	 */
+	public void setDirection(int direction){
+	}
+	
+	/*
+	 * @fps - frame rate	
+	 *
+	 * Update Super Breakout View of the Bat  and change the Bat's coordinate
+	 * based on the movement chosen.
+	 */
+	public void update(long fps){}	
 }
