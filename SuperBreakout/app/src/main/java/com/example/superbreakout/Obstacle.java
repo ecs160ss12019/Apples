38b15919dbc67a485d837cce6c4962a68107da9e
Nicholas Steven <nicholassteven998@gmail.com>
2019-07-16 13:38:45 -0700

    Commented to be implemented code

--- SuperBreakout/app/src/main/java/com/example/superbreakout/Obstacle.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/Obstacle.java
@@ -49,38 +49,47 @@ public class Obstacle extends GameObject {
         return isVisible;
     }
 
+    /**
+     * TODO
+     * Implement durability and effects
+     */
+    /*
     public void reduceDurability() {
         this.durability -= 1;
-    }
+    } */
 
     /*
      *   This function fetches the current durability of the obstacle.
      *   May prove useful when generating varying durabilities.
      */
+    /*
     public int getDurability() {
         return this.durability;
-    }
+    } */
 
     /*
      *   This function generates a Debris object within the game
      *   once the obstacle has been destroyed.
      *   The obstacle may or may not drop a Debris object.
      */
+    /*
     public void generateDebris() {
 
-    }
+    } */
 
     /*
      *   This function returns the String name of the effect
      */
+    /*
     public String getEffect() {
         return this.effect;
-    }
+    } */
 
     /*
      *   This function applies an effect to the obstacle
      *   given by effect input provided by another obstacle
      */
+    /*
     public void applyEffect(String effect) {
         switch (effect) {
             case "effectName":
@@ -89,5 +98,5 @@ public class Obstacle extends GameObject {
             default:
                 break;
       }
-    }
+    } */
 }
