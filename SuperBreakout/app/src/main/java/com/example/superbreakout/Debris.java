c1fb45bc2581160c9055cc46324a3db429f72307
billpansacola <billmatthew.pansacola@gmail.com>
2019-07-16 17:30:09 -0700

    committed changes from development-debris/bill to master

--- SuperBreakout/app/src/main/java/com/example/superbreakout/Debris.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/Debris.java
@@ -1,77 +1,111 @@
 package com.example.superbreakout;
+
 import android.graphics.RectF;
+import java.util.Random;
 
 /*
  *  Falling objects that can destroy the bat, or grant either an upgrade or downgrade
  */
 public class Debris {
 
-    private final int SPEED = 1; // Constant speed (in Y-direction) of debris
+    private final float SPEED = 2.5f; // Constant speed (in Y-direction) of debris
 
-    private RectF debris; // Square/Rect representing the falling debris
-    private float width;  // Debris' width in terms of pixels
-    private float height; // Debris' height in terms of pixels
+    private RectF rect; // Square/Rect representing the falling debris
 
-    private float xPos; // x coordinate of debris
-    private float yPos; // y coordinate of debris
+    private String debrisType; // Debris type (Harmful, Upgrade, Downgrade, None)
 
-    private String debrisType; // Debris type (Harmful, Upgrade, Downgrade)
-
+    private boolean active; // If the debris is in frame or not
 
     /*
      * Debris constructor that takes in the initial coordinates
      * of a destroyed obstacle.
      */
-    public Debris() {
-        // sets debrisType
-    }
+    public Debris(int row, int column, int width, int height) {
 
-    /* This function updates the new position of the debris
-     * for the draw function in SuperBreakoutGame to draw
-     * the new position of the debris
-     */
-    public void update(long fps) {}
+        int padding = height/5;
+
+        rect = new RectF(column * width + padding,
+                row * height + padding,
+                column * width + width - padding,
+                row * height + height - padding);
+
+
+        // sets the debris type randomly from the 4 types.
+        String[] types = {"Harmful", "Upgrade", "Downgrade", "None", "None", "None"};
+        Random random = new Random();
+
+        debrisType = types[random.nextInt(types.length)];
+
+        active = false;
+    }
 
     /*
      *  This function generates a debris object once an obstacle is destroyed
      */
-    /*public RectF spawnDebris() {
-        switch(this.debrisType) {
-            case "Harmful":
-                // spawn harmful debris that destroys bat
-                break;
-            case "Upgrade":
-                // spawn random upgrade
-                break;
-            case "Downgrade":
-                // spawn random downgrade
-                break;
-        }
-        // returns this.debris depending on its type
-    }*/
+    public RectF getRect() {
+        return this.rect;
+    }
 
-    /*
-     *  Enables the debris to fall once it spawns
-     */
-    public void fall() {
-        // decrease the debris' yPos depending on SPEED
+    public String getDebrisType() {
+        return debrisType;
+    }
+
+    public boolean getActive() {
+        return active;
     }
 
     /*
-     *  Destroys the object once it comes in contact
-     *  with the ball, bat, or bottom of the frame
+     *  This method is used once the ball hits an obstacle
      */
-    public void destroy() {
-        // remove debris from frame/game
+    public void activate() {
+        active = true;
     }
 
     /*
-     *  Checks whether the debris is hit by the bat or ball
+     * this method is used once the ball hits a debris
      */
-    public void hit() {
-        // if hit by the ball, call destroy()
-        // if hit by the bat, apply effects depending on debrisType
+    public void deactivate() {
+        active = false;
     }
+
+    /*
+     * The update method will change the top/bottom coordinates of debris\
+     * to imitate an object "falling" to the bottom of the screen.
+     */
+    public void update(long fps) {
+        rect.top = rect.top + SPEED;
+        rect.bottom = rect.bottom + SPEED;
+    }
+
+//    /* This function updates the new position of the debris
+//     * for the draw function in SuperBreakoutGame to draw
+//     * the new position of the debris
+//     */
+//    public void update(long fps) {}
+//
+//
+//    /*
+//     *  Enables the debris to fall once it spawns
+//     */
+//    public void fall() {
+//        // decrease the debris' yPos depending on SPEED
+//    }
+//
+//    /*
+//     *  Destroys the object once it comes in contact
+//     *  with the ball, bat, or bottom of the frame
+//     */
+//    public void destroy() {
+//        // remove debris from frame/game
+//    }
+//
+//    /*
+//     *  Checks whether the debris is hit by the bat or ball
+//     */
+//    public void hit() {
+//        // if hit by the ball, call destroy()
+//        // if hit by the bat, apply effects depending on debrisType
+//    }
 
 
 }
