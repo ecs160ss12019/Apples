5a62fc30a18d97d5a683c5fee997d5eeeac87328
billpansacola <billmatthew.pansacola@gmail.com>
2019-07-19 20:21:58 -0700

    created Upgrade and Downgrade class derived from Effect class.

--- SuperBreakout/app/src/main/java/com/example/superbreakout/Effect.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/Effect.java
@@ -1,11 +1,30 @@
 package com.example.superbreakout;
 
 public class Effect {
+
     // parameters that check whether the Effect will be for the ball or the bat
     private boolean ballEffect;
     private boolean batEffect;
 
     public Effect() {
         // Randomly decide which component (bat or ball) to affect
+    }
+}
+
+class Upgrade extends Effect {
+
+    public String upgradeName; // Name of the upgrade
+
+    public Upgrade() {
+        // Randomly generate upgrade based on Effect booleans
+    }
+}
+
+class Downgrade extends Effect {
+
+    public String downgradeName; // Name of the downgrade
+
+    public Downgrade() {
+        // Randomly generate downgrade based on Effect booleans
     }
 }
