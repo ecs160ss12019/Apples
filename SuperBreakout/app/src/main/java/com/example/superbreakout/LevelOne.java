aee24cbf53dc03ce3429de0464515d74fa0f4c17
Nicholas Steven <nicholassteven998@gmail.com>
2019-07-22 16:42:11 -0700

    Fixed level one's durability settings

--- SuperBreakout/app/src/main/java/com/example/superbreakout/LevelOne.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/LevelOne.java
@@ -12,6 +12,7 @@ public class LevelOne extends Level {
     public LevelOne(int x, int y){
         super(x,y);
         level = LEVEL_ONE;
+        randomizer = new Randomizer();
     }
 
     @Override
@@ -24,6 +25,13 @@ public class LevelOne extends Level {
         for (int column = 0; column < 8; column++) {
             for (int row = 0; row < 3; row++) {
                 bricks[numBricks] = new Obstacle(context, row, column, brickWidth, brickHeight);
+
+                if (randomizer.getRandBoolean()) {
+                    bricks[numBricks].setDurability(randomizer.getRandNumber(1,3));
+                }
+                else {
+                    bricks[numBricks].setDurability(0);
+                }
                 // can possibly change this to spawnDebris()
                 debris[numBricks] = new Debris(row, column, brickWidth, brickHeight);
                 numBricks++;
