45240a341bbd5cac278c75400364a8f46c946ced
Nicholas Steven <nicholassteven998@gmail.com>
2019-07-21 22:25:50 -0700

    Abstract randomizer class and implemented getRandBoolean()

--- SuperBreakout/app/src/main/java/com/example/superbreakout/Randomizer.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/Randomizer.java
@@ -1,3 +1,7 @@
+/**
+ * Randomizer class to generate random numbers
+ */
+
 package com.example.superbreakout;
 
 import java.util.Random;
@@ -5,13 +9,24 @@ import java.util.Random;
 public class Randomizer {
     int high, low;
 
-    public Randomizer(int min, int max) {
-        this.high = max;
-        this.low = min;
+    public Randomizer() {
+        this.high = 0;
+        this.low = 0;
     }
 
-    public int getRandNumber() {
+    // generates random integer
+    public int getRandNumber(int min, int max) {
+        this.high = max;
+        this.low = min;
+
         Random generator = new Random();
         return generator.nextInt(high - low) + low;
+    }
+
+    // generates true or false
+    public boolean getRandBoolean() {
+        Random generator = new Random();
+
+        return generator.nextBoolean();
     }
 }
