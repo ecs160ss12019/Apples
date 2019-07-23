0edc044d8feb763dff26f2b83042cf503be8ee4d
vincentsheng <shengyeesiow96@gmail.com>
2019-07-23 15:37:34 -0700

    changed resume button feature to main menu return
    works like a charm

--- SuperBreakout/app/src/main/java/com/example/superbreakout/PauseMenuPopUp.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/PauseMenuPopUp.java
@@ -1,8 +1,11 @@
 package com.example.superbreakout;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 
 public class PauseMenuPopUp extends Activity {
 
@@ -18,6 +21,16 @@ public class PauseMenuPopUp extends Activity {
 
         int width = dm.widthPixels;
         int height = dm.heightPixels;
+
+        final Button mainMenuButton = findViewById(R.id.mainMenuButton);
+        mainMenuButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(PauseMenuPopUp.this, SuperBreakoutActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         getWindow().setLayout((int) (width*.8), (int) (height*.8));
     }
