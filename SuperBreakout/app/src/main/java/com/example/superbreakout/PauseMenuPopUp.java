266ca3a6f32305c9155fb30926bb5a792c60c98b
Nicholas Steven <nicholassteven998@gmail.com>
2019-07-23 23:26:13 -0700

    Added Settings Menu

--- SuperBreakout/app/src/main/java/com/example/superbreakout/PauseMenuPopUp.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/PauseMenuPopUp.java
@@ -3,6 +3,7 @@ package com.example.superbreakout;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
@@ -36,6 +37,8 @@ public class PauseMenuPopUp extends Activity {
         settingsButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //settings (for Steven)
+                startActivity(new Intent(PauseMenuPopUp.this, SettingsMenuPopUp.class));
+                finish();
             }
         });
 
