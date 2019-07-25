264506a8eef2e32e9f7d272a067d7f59b361fb44
billpansacola <billmatthew.pansacola@gmail.com>
2019-07-24 20:48:33 -0700

    Bug should be commented out

--- SuperBreakout/app/src/main/java/com/example/superbreakout/Level.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/Level.java
@@ -18,6 +18,8 @@ public abstract class Level {
     int level = 0;
     Obstacle[] bricks = new Obstacle[bricksInLevel];
     Debris[] debris = new Debris[bricksInLevel];
+//    Upgrade[] ug = new Upgrade[bricksInLevel];
+//    Downgrade[] dg = new Downgrade[bricksInLevel];
     Context context;
 
     Randomizer randomizer;
@@ -85,6 +87,14 @@ public abstract class Level {
 
                     if(!debris[i].getDebrisType().equals("None")) {
                         debris[i].activate();
+
+                        // BUG
+//                        if(debris[i].getDebrisType().equals("Upgrade")) {
+//                            ug[i] = new Upgrade();
+//                        } else if(debris[i].getDebrisType().equals("Downgrade")) {
+//                            dg[i] = new Downgrade();
+//                        }
+
                     }
                     ball.reverseYVelocity();
                     hit = true;
@@ -108,12 +118,15 @@ public abstract class Level {
                     switch(debris[i].getDebrisType()) {
                         case "Harmful":
                             // do something
+                            bat.stun();
                             break;
                         case "Upgrade":
-                            // do something
+                            Upgrade ugs = new Upgrade();
+                            applyUpgrade(ugs, ball, bat);
                             break;
                         case "Downgrade":
-                            // do something
+                            Downgrade dgs = new Downgrade();
+                            applyDowngrade(dgs, ball, bat);
                             break;
                     }
 
@@ -144,6 +157,28 @@ public abstract class Level {
             if(debris[i].getActive()) {
                 debris[i].update();
             }
+        }
+    }
+
+    private void applyUpgrade(Upgrade ug, Ball ball, Bat bat) {
+        switch(ug.getEffectTarget()) {
+            case "Ball":
+                ball.applyUpgrade(ug.upgradeName);
+                break;
+            case "Bat":
+                bat.applyUpgrade(ug.upgradeName);
+                break;
+        }
+    }
+
+    private void applyDowngrade(Downgrade dg, Ball ball, Bat bat) {
+        switch(dg.getEffectTarget()) {
+            case "Ball":
+                ball.applyDowngrade(dg.downgradeName);
+                break;
+            case "Bat":
+                bat.applyDowngrade(dg.downgradeName);
+                break;
         }
     }
 }
