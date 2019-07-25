3a6b4d112e264ef3bd16fe7fe2ede4f9b698fb2b
Nicholas Steven <nicholassteven998@gmail.com>
2019-07-24 21:22:43 -0700

    Cleanup

--- SuperBreakout/app/src/main/java/com/example/superbreakout/PauseMenuPopUp.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/PauseMenuPopUp.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 
 public class PauseMenuPopUp extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
