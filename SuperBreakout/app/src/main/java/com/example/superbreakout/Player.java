40810d17518be17a982daa1844601acefd7b6853
annguyen120 <antnguye@ucdavis.edu>
2019-07-25 19:50:49 -0700

    Fixed point system to add new life per 300 points

--- SuperBreakout/app/src/main/java/com/example/superbreakout/Player.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/Player.java
@@ -3,8 +3,10 @@ package com.example.superbreakout;
 public class Player {
 
     private final int STARTING_LIVES = 3;
+    private final int FREE_LIFE_POINTS = 100;
     int score = 0;
     int consecutiveHits = 0;
+    int nextLife = 0;
     int lives;
 
     public Player(){
@@ -20,10 +22,18 @@ public class Player {
     public void hitBrick(){
         consecutiveHits += 1;
         score += (int)(10.0*(float)(consecutiveHits/10.0));
+        nextLife += (int)(10.0*(float)(consecutiveHits/10.0));
+        if(nextLife >= FREE_LIFE_POINTS){
+            int residualPoints = nextLife%FREE_LIFE_POINTS;
+            lives += nextLife/FREE_LIFE_POINTS;
+            nextLife = residualPoints;
+        }
     }
+
     public void missBrick(){
         consecutiveHits = 0;
     }
+
     public boolean isAlive(){
         if(lives == 0) return false;
         else return true;
