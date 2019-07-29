7c4d1e43ed3554f206a74209a06506a9d31eebdd
Nicholas Steven <nicholassteven998@gmail.com>
2019-07-29 15:57:37 -0700

    Create BackgroundMusic

---
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/BackgroundMusic.java
@@ -0,0 +1,68 @@
+package com.example.superbreakout;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class BackgroundMusic extends Service {
+    private static final String TAG = null;
+    MediaPlayer player;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        player = MediaPlayer.create(this, R.raw.summer);
+        player.setLooping(true);
+        player.setVolume(100,100);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.stop();
+        player.release();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+    }
+}
