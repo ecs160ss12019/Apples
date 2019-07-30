797c590aa2b18985fd47e82ae073d30aa59dba05
vincentsheng <shengyeesiow96@gmail.com>
2019-07-30 14:51:35 -0700

    leaderboard xml added

---
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/Leaderboard.java
@@ -0,0 +1,17 @@
+package com.example.superbreakout;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Leaderboard extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.leaderboard_activity);
+    }
+
+
+}
--- SuperBreakout/app/src/main/java/com/example/superbreakout/PauseMenuPopUp.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/PauseMenuPopUp.java
@@ -44,6 +44,8 @@ public class PauseMenuPopUp extends Activity {
         leaderboardButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //retrieve leaderboard
+                Intent hsIntent = new Intent(PauseMenuPopUp.this, Leaderboard.class);
+                startActivity(hsIntent);
             }
         });
 
