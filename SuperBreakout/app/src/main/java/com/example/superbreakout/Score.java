452c3dc0f7bd8cb2f6d20cabe67cb4c0714948b7
vincentsheng <shengyeesiow96@gmail.com>
2019-07-30 13:36:28 -0700

    Score class completed

--- SuperBreakout/app/src/main/java/com/example/superbreakout/Score.java
+++ SuperBreakout/app/src/main/java/com/example/superbreakout/Score.java
@@ -11,8 +11,15 @@ public class Score implements Comparable<Score> {
         scoreValue = value;
     }
 
+    // return 0 if equal
+    // return 1 if parameter is greater than this score
+    // return -1 if parameter is less than this score
     public int compareTo(Score score) {
-        
+        return score.scoreValue > scoreValue? 1: score.scoreValue < scoreValue? -1 : 0;
+    }
+
+    public String getScoreText() {
+        return playerName + " - " + scoreValue;
     }
 
 
